9e311f785466ffd8e0a37aa1b2b0869477dccdf8
samarthbanodia <sageofthesixpaths122@gmail.com>
2025-01-02 13:21:23 +0530

    after fixing build error

--- components/AdSense.tsx
+++ components/AdSense.tsx
@@ -1,19 +1,3 @@
-import Script from 'next/script';
-import React from 'react'
-
-type AdsenseTypes = {
-    pId: string;
+export default function Adsense({ pId }: { pId: string }) {
+  return null;
 }
-
-const AdSense = ({ pId }: AdsenseTypes) => {
-  return (
-    <Script
-        async
-        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
-        crossOrigin='anonymous'
-        strategy='afterInteractive'
-    />
-  )
-}
-
-export default AdSense
